d8b6fff0130685f81014aee432bf58d294c8806d
Mostafa Elbeih <mte10@mail.aub.edu>
2020-03-04 21:36:01 +0200

    second commit

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
   res.send("HEllo world");
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("Server started on port " + port));
