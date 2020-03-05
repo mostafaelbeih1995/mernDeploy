c0edbc312c26dcfabd854e71e1d057c331ce6a52
Mostafa Elbeih <mte10@mail.aub.edu>
2020-03-05 18:38:44 +0200

    new commit

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.get("/api/test", (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
     // app.use(express.static('client/build'));
-    app.get("/", (req, res) => {
+    app.get("*", (req, res) => {
         // res.send("In production mode");
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+        res.sendFile(path.join(__dirname, "client/build", "index.html"));
     });
 //   app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
