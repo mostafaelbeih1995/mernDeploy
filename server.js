b94d6e1e374c6c79c17e7fe930695ed2da142938
Mostafa Elbeih <mte10@mail.aub.edu>
2020-03-05 12:09:27 +0200

    ya allah it should work

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -17,15 +18,17 @@ app.use(cors());
 // else {
 //     res.send("Not in production");
 // }
-if (process.env.NODE_ENV === 'production') {
-    // app.get("/", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    // //   res.send("Fuck Yall");
-    // });
-    app.use(express.static("client/build"));
-}
+
+app.use(express.static(path.join(__dirname,"client", "build" , "index.html")));
+
+app.get("/api/test", (req, res) => {
+    res.send("Test end point")
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("Server started on port " + port));
